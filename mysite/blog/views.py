7a39c6ddf05084dd59412c99208118057c55f96b
DAEDALUS <tdubinchik3@gmail.com>
2021-08-18 12:49:58 +0300

    Change function post_list on class PostListView

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render, get_object_or_404
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from .models import Post
 
@@ -9,8 +8,6 @@ class PostListView(ListView):
     context_object_name = 'posts'
     paginate_by = 5
     template_name = 'blog/post/list.html'
-
-
 
 
 def post_detail(request, year, month, day, post):
@@ -23,8 +20,3 @@ def post_detail(request, year, month, day, post):
                   {'post': post})
 
 
-class PostListView(ListView):
-    queryset = Post.published.all()
-    context_object_name = 'posts'
-    paginate_by = 3
-    template_name = 'blog/post/list.html'
