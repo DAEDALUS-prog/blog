79430f82f3eb06d7574950ff24b757d7d0e74429
DAEDALUS <tdubinchik3@gmail.com>
2021-08-22 00:37:26 +0300

    Add share email

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -29,17 +29,20 @@ def post_share(request, post_id):
         form = EmailPostForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
-            post_url = request.build_absolute_url(post.get_absolute_url())
+            post_url = request.build_absolute_uri(post.get_absolute_url())
             subject = '{} ({}) recommends you reading " {}"'\
                 .format(cd['name'], cd['email'], post.title)
             message = 'Read "{}" at {}\n\n{}\'s comments:{}'\
                 .format(post.title, post_url, cd['name'], cd['comments'])
             send_mail(subject, message, 'tdubinchik3@gmail.com', [cd['to']])
             sent = True
-        else:
-            form = EmailPostForm()
-            return render(request, 'blog/post/share.html',
+        return render(request, 'blog/post/share.html',
+                      {'post': post, 'form': form, 'sent': sent},
+                      )
+    else:
+        form = EmailPostForm()
+        return render(request, 'blog/post/share.html',
                           {'post': post, 'form': form, 'sent': sent},
-                          )
+                      )
 
 
