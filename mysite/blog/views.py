42f953fb1bb1664eb306bd4b132fdaa137d06b10
DAEDALUS <tdubinchik3@gmail.com>
2021-08-21 23:29:41 +0300

    create func post_share in views.py

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
 from .models import Post
+from .forms import EmailPostForm
 
 
 class PostListView(ListView):
@@ -18,5 +19,18 @@ def post_detail(request, year, month, day, post):
     return render(request,
                   'blog/post/detail.html',
                   {'post': post})
+
+
+def post_share(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status='draft')
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+        else:
+            form = EmailPostForm()
+            return render(request, 'blog/post/share.html',
+                          {'post': post, 'form': form},
+                          )
 
 
