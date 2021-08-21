614dc3e0bb8d94ff278396083842e0ac57f1e865
DAEDALUS <tdubinchik3@gmail.com>
2021-08-21 23:52:55 +0300

    Add functional mail

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -4,6 +4,7 @@ app_name = 'blog'
 urlpatterns = [
  # post views
  path('', views.PostListView.as_view(), name='post_list'),
- path('<int:year>/<int:month>/<int:day>/<slug:post>/',
- views.post_detail, name='post_detail'),
+ path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail'),
+ path('<int:post_id>/share/', views.post_share, name='post_share'),
+
 ]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,4 @@
+from django.core.mail import send_mail
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
 from .models import Post
@@ -23,14 +24,22 @@ def post_detail(request, year, month, day, post):
 
 def post_share(request, post_id):
     post = get_object_or_404(Post, id=post_id, status='draft')
+    sent = False
     if request.method == 'POST':
         form = EmailPostForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
+            post_url = request.build_absolute_url(post.get_absolute_url())
+            subject = '{} ({}) recommends you reading " {}"'\
+                .format(cd['name'], cd['email'], post.title)
+            message = 'Read "{}" at {}\n\n{}\'s comments:{}'\
+                .format(post.title, post_url, cd['name'], cd['comments'])
+            send_mail(subject, message, 'tdubinchik3@gmail.com', [cd['to']])
+            sent = True
         else:
             form = EmailPostForm()
             return render(request, 'blog/post/share.html',
-                          {'post': post, 'form': form},
+                          {'post': post, 'form': form, 'sent': sent},
                           )
 
 
