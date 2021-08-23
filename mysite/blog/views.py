7f72e771f65b4ab5a09e6abb8d0d9dca424d1e4f
Tim <tdubinchik3@gmail.com>
2021-08-24 02:33:49 +0300

    Add -similar posts-

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post
 from .forms import EmailPostForm, CommentForm
 from taggit.models import Tag
-
+from django.db.models import Count
 
 def post_list(request, tag_slug=None):
     object_list = Post.published.all()
@@ -43,11 +43,18 @@ def post_detail(request, year, month, day, post):
         else:
             comment_form = CommentForm()
 
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids)\
+        .exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags'))\
+        .order_by('-same_tags', '-publish')[:4]
+
     return render(request, 'blog/post/detail.html',
                               {'post': post,
                                'comments': comments,
                                'comment_form': comment_form,
-                               'new_comment': new_comment})
+                               'new_comment': new_comment,
+                               'similar_posts': similar_posts})
 
 
 def post_share(request, post_id):
