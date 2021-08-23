03dc761e106f3b64cbe362b3cc8b4fe1673f4700
Tim <tdubinchik3@gmail.com>
2021-08-23 17:30:05 +0300

    processing of model forms - comments

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,8 +1,8 @@
 from django.core.mail import send_mail
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
-from .models import Post
-from .forms import EmailPostForm
+from .models import Post, Comment
+from .forms import EmailPostForm, CommentForm
 
 
 class PostListView(ListView):
@@ -17,9 +17,23 @@ def post_detail(request, year, month, day, post):
                              publish__year=year,
                              publish__month=month,
                              publish__day=day)
+    comments = post.comments.filter(active=True)
+    new_comment = None
+
+    if request.method == 'POST':
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.post = post
+            new_comment.save()
+        else:
+            comment_form = CommentForm()
     return render(request,
                   'blog/post/detail.html',
-                  {'post': post})
+                  {'post': post,
+                   'comments': comments,
+                   'comment_form': comment_form,
+                   'new_comment': new_comment})
 
 
 def post_share(request, post_id):
