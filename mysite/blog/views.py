37cb4953df5f02f2e9c545af0a2a4e1fe4e3eda8
Tim <tdubinchik3@gmail.com>
2021-08-26 00:43:29 +0300

    Create -site maps-

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -6,6 +6,7 @@ from .forms import EmailPostForm, CommentForm
 from taggit.models import Tag
 from django.db.models import Count
 
+
 def post_list(request, tag_slug=None):
     object_list = Post.published.all()
     tag = None
